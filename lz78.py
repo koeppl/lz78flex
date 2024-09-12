53f45ac07cd58c2ac111d2c9f6324481aa99ee0f
Dominik Köppl <git@dkppl.de>
2024-09-12 10:00:00 +0900

    no debug speedup

--- lz78.py
+++ lz78.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
-""" abook replacement """
+""" lz78 parsing and flexible lz78 parsing """
 # pylint: disable=bad-indentation,line-too-long,invalid-name
 
 import sys
 import logging
 
 def lz78(text):
+	""" lz78 parsing """
 	dictionary = {}
 	parent_id = 0
-	factor = ''
+	current_factor = ''
 	for char in text:
-		factor += char
-		if factor not in dictionary:
-			dictionary[factor] = len(dictionary) + 1
-			logging.debug(f'({parent_id},"{factor[-1]}") : {factor} -> {dictionary[factor]}')
-			factor = ''
+		current_factor += char
+		if current_factor not in dictionary:
+			dictionary[current_factor] = len(dictionary) + 1
+			logging.debug('(%d, %s) : %s -> %d',  parent_id, current_factor[-1], current_factor, dictionary[current_factor])
+			current_factor = ''
 		else:
-			parent_id = dictionary[factor]
+			parent_id = dictionary[current_factor]
 	return len(dictionary)
 
 def longest_prefix(dictionary, text):
@@ -37,21 +38,21 @@ def lz78flex(text):
 	while textpos < len(text):
 		potential_factor = longest_prefix(dictionary, text[textpos:])
 
-		first_length = len(potential_factor)+1
-		second_length = 0
-		if first_length < len(text[textpos:]):
+		current_length = len(potential_factor)+1
+		next_length = 0
+		if current_length < len(text[textpos:]):
 			for i in range(1,len(potential_factor)+2):
 				potential_subsequent_factor = longest_prefix(dictionary, text[textpos+i:])
-				if len(potential_subsequent_factor) >= second_length:
-					first_length = i
-					second_length = len(potential_subsequent_factor)
+				if len(potential_subsequent_factor) >= next_length:
+					current_length = i
+					next_length = len(potential_subsequent_factor)
 
-		first_factor = text[textpos:textpos+first_length]
-		if first_factor not in dictionary:
-			dictionary[first_factor] = len(dictionary) + 1
-		parent_id = dictionary[first_factor[:-1]] if len(first_factor) >= 2 else 0
-		logging.debug(f'({parent_id},"{first_factor[-1]}") : {first_factor} -> {dictionary[first_factor]}')
-		textpos += first_length
+		current_factor = text[textpos:textpos+current_length]
+		if current_factor not in dictionary:
+			dictionary[current_factor] = len(dictionary) + 1
+		parent_id = dictionary[current_factor[:-1]] if len(current_factor) >= 2 else 0
+		logging.debug('(%d, %s) : %s -> %d',  parent_id, current_factor[-1], current_factor, dictionary[current_factor])
+		textpos += current_length
 	return len(dictionary)
 
 
@@ -61,13 +62,15 @@ if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		filename = sys.argv[1]
 		with open(filename,encoding='utf-8') as file:
-			text = file.read()
+			inputtext = file.read()
 	else:
-		text = sys.stdin.read()
+		inputtext = sys.stdin.read()
 
-	# text='abababcabcdabcdeabcdefabcdefgabcdefgabcdefgabcdefgabcdefgabcdefgabcdefg'
 	logging.debug('78')
-	lz78phrases = lz78(text)
+	lz78phrases = lz78(inputtext)
 	logging.debug('flex')
-	flex_phrases = lz78flex(text)
-	print(f'file={filename} length={len(text)} factors78={lz78phrases} factorsflex={flex_phrases}')
+	flex_phrases = lz78flex(inputtext)
+	print(f'file={filename} length={len(inputtext)} factors78={lz78phrases} factorsflex={flex_phrases}')
+
+
+# inputtext='abababcabcdabcdeabcdefabcdefgabcdefgabcdefgabcdefgabcdefgabcdefgabcdefg'
